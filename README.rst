ebd4233033efcc2102708172bf531bb0e06b8cff
akula <shane.liu@endurance.com>
2017-07-17 17:05:44 -0600

    initial

---
+++ README.rst
@@ -0,0 +1,12 @@
+===
+EVA
+===
+
+
+Evolving Virtual Assistant
+
+
+Description
+===========
+
+Stands for Evolving Virtual Assistant. I hesitate to use A.I. here; more along the lines of a culmination of helpful functionalities, eventually to turn into an A.I. ¯\_(ツ)_/¯
---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from eva.skeleton import fib
+
+__author__ = "4kula"
+__copyright__ = "4kula"
+__license__ = "gpl3"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
